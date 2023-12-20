96755a893d84a6e139e6669cfd98e85f9e2e9daa
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2023-12-19 20:35:55 -0500

    updated tic tac toe grid

---
+++ app.js
@@ -0,0 +1,18 @@
+const board = document.getElementbyId('.button-select')
+let restartBtn = document.getElementbyId('restart')
+
+let winningPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// Player 'X' goes first
+let xTurn = true;
+let count = 0;
+
