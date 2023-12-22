b587bbd0a2c0837c8878fd771aad91b82e8a57bd
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2023-12-21 21:28:14 -0500

    adding logic

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const board = document.getElementbyId('.button-select')
-let restartBtn = document.getElementbyId('restart')
+const board = document.getElementById('button-select')
+let newGameBtn = document.getElementById('new-game')
+let restartBtn = document.getElementById('restart')
 const players = ['X', 'O']
 let currentPlayer = players[0]
+const winningMessage = document.getElementById('winningMessage')
+const winningMessageTextElement = document.getElementById('winningMessageText')
 
+// Winning Pattern
 let winningPattern = [
     [0, 1, 2],
     [0, 3, 6],
@@ -18,3 +22,61 @@ let winningPattern = [
 let xTurn = true;
 let count = 0;
 
+// Button Disable All (start and restart)
+const disableButtons = () => {
+    btnRef.forEach(element => (element.disabled = true)); {
+        // popup enabled
+        popupRef.classList.remove('hide');
+    };
+
+    // enable all buttons (for new game and restart)
+const enableButtons = () => {
+    board.forEach((element) => {
+        element.innerText = "";
+        element.disabled = false;
+    });
+    // popup disabled
+    popupRef.classList.add('hide');
+};
+}
+
+// New Game
+newGameBtn.addEventListener('click', () => {
+    count = 0
+    enableButtons();
+});
+
+restartBtn.addEventListener('click', () => {
+    count = 0
+    enableButtons();
+});
+
+
+// Display X/O with click
+board.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (xTurn) {
+            xTurn = false;
+            // Display X
+            element.innerText = "X";
+            element.disabled = true;
+        } else {
+            xTurn = true;
+            // Display O
+            element.innerText = "O";
+            element.disabled = true;
+        }
+        count += 1;
+        if (count == 9) {
+            drawFunction();
+        }
+        // each click check for win
+        winChecker();
+    });
+});
+
+
+
+
+
+window.onload = enableButtons;
