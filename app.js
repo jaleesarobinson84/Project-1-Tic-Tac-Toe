08089ef8cd7c9d00c0884df09f190d5fc0553ea3
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2023-12-28 17:39:59 -0500

    updating all styles and functions

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-let board = Array.from(document.querySelectorAll('.button'))
-let playerText = document.getElementById('playerText')
-let restartBtn = document.querySelector('#restartBtn')
+const board = (document.querySelectorAll('button'));
+const playerText = document.getElementById('playerText');
+const restartBtn = document.getElementById('#restartBtn');
 
-const playerX = "x"
-const playerO = "o"
-let currentPlayer = playerX
-let spaces = Array(9).fill(null)
-
-const startGame = () => {
-    board.forEach(button => button.addEventListener('click', buttonclicked))
-}
-
-function buttonclicked(e) {
-   const button = e.target.button
-
-   if(!spaces[button]) {
-    spaces[button] = currentPlayer
-    e.target.innerText = currentPlayer
-
-    if(playerWon() !==false) {
-        playerText = `${currentPlayer} has won!`
-        
-    }
-
-    currentPlayer = currentPlayer == playerX ? playerO : playerX
-   }
-}
-
-const winningPattern = [
+let winningPattern = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -36,32 +11,108 @@ const winningPattern = [
     [2,5,8],
     [0,4,8],
     [2,4,6]
-]
+];
 
-function playerWon() {
-    for(const condition of winningPattern) {
-        let [a, b, c] = condition
-    
-        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-            return[a,b,c]
+let turnX = true;
+let count = 0;
+
+const winCheck = () => {
+    for (let i of winningPattern) {
+        let [element1, element2, element3] = [
+            board[i[0]].innerText,
+            board[i[1]].innerText,
+            board[i[2]].innerText,
+        ];
+        if (element1 != "" && (element2 != "") & (element3 != "")) {
+            if (element1 == element2 && element2 == element3) {
+                
+                winFunction(element1);
+            }
         }
     }
-return false
+};
+
+board.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (turnX) {
+            turnX = false;
+            // X display
+            element.innerText = 'X';
+            element.disabled = true;
+        } else {
+            turnX = true;
+            // O Display
+            element.innerText = "O";
+            element.disabled = true;
+        }
+        count += 1;
+        if(count === 9)
+    }
+})
+
+const disableButtons = () => {
+    board.forEach((button) => (button.disabled = true));
 }
 
-restartBtn.addEventListener('click', restart)
-
-function restart() {
-    spaces.fill(null)
-
-    board.forEach( button => {
-        button.innerText = ''
-    })
-
-    playerText = 'Tic Tac Toe'
-    
-    currentPlayer = playerX
+const enableButtons = () => {
+    board.forEach((element) => {
+        element.innerText = "";
+        element.disabled = false;
+    });
 }
 
-startGame()
+const winFunction = (letter) => {
+    disableButtons();
+    if (letter == "X") {
+        msgRef.innerHTML = "X Wins";
+    } else {
+        msgRef.innerHTML = "O Wins"
+    }
+};
+
+const drawFunction = () => {
+    disableButtons();
+    msgRef.innerHTML = "It's a Draw"
+};
+
+restartBtn.addEventListener("click", () => {
+    count = 0
+    enableButtons();
+});
+
+// const winCheck = () => {
+//     for (let i of winningPattern) {
+//         let [element1, element2, element3] = [
+//             board[i[0]].innerText,
+//             board[i[1]].innerText,
+//             board[i[2]].innerText,
+//         ];
+//         if (element1 != "" && (element2 != "") & (element3 != "")) {
+//             if (element1 == element2 && element2 == element3) {
+//                 winFunction(element1)
+//             }
+//         }
+//     }
+// };
+
+ board.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (turnX) {
+            turnX = false;
+            element.innerText = "X";
+            element.disabled =  true;
+        } else {
+            turnX = true;
+            element.innerText = "O";
+            element.disabled = true;
+        }
+        count += 1;
+        if (count == 9) {
+            drawFunction();
+        }
+        winCheck();
+        });
+    });
+ 
+    window.onload = enableButtons;
 
