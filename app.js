6eba903518a20e53aa39c2f4b243e09b7bea01fb
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-01 13:40:19 -0500

    updating logic

--- app.js
+++ app.js
@@ -1,118 +1,62 @@
-const board = (document.querySelectorAll('button'));
-const playerText = document.getElementById('playerText');
-const restartBtn = document.getElementById('#restartBtn');
+const board = document.querySelectorAll("button");
+const restartBtn = document.querySelector("restart")
+const resultBox = document.querySelector(".result-box")
+const wonText = resultBox.querySelector("won-text")
 
-let winningPattern = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
+let winningCombo = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-let turnX = true;
+let playerX = true;
 let count = 0;
 
-const winCheck = () => {
-    for (let i of winningPattern) {
-        let [element1, element2, element3] = [
-            board[i[0]].innerText,
-            board[i[1]].innerText,
-            board[i[2]].innerText,
-        ];
-        if (element1 != "" && (element2 != "") & (element3 != "")) {
-            if (element1 == element2 && element2 == element3) {
-                
-                winFunction(element1);
-            }
-        }
-    }
-};
 
-board.forEach((element) => {
+
+const winChecker = () => {
+
+    for (let i of winningCombo) {
+      let [element1, element2, element3] = [
+        board[i[0]].innerText,
+        board[i[1]].innerText,
+        board[i[2]].innerText,
+      ];
+       
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+        if (element1 == element2 && element2 == element3) {
+          
+          winFunction(element1);
+        }
+      }
+    }
+  };
+  board.forEach((element) => {
     element.addEventListener("click", () => {
-        if (turnX) {
-            turnX = false;
-            // X display
-            element.innerText = 'X';
-            element.disabled = true;
-        } else {
-            turnX = true;
-            // O Display
-            element.innerText = "O";
-            element.disabled = true;
-        }
-        count += 1;
-        if(count === 9)
-    }
-})
-
-const disableButtons = () => {
-    board.forEach((button) => (button.disabled = true));
-}
-
-const enableButtons = () => {
-    board.forEach((element) => {
-        element.innerText = "";
-        element.disabled = false;
+      if (playerX) {
+        playerX = false;
+        
+        element.innerText = "X";
+        element.disabled = true;
+      } else {
+        playerX = true;
+        
+        element.innerText = "O";
+        element.disabled = true;
+      }
+      
+      count += 1;
+      if (count == 9) {
+        drawFunction();
+      }
+      
+      winChecker();
     });
-}
+  });
 
-const winFunction = (letter) => {
-    disableButtons();
-    if (letter == "X") {
-        msgRef.innerHTML = "X Wins";
-    } else {
-        msgRef.innerHTML = "O Wins"
-    }
-};
-
-const drawFunction = () => {
-    disableButtons();
-    msgRef.innerHTML = "It's a Draw"
-};
-
-restartBtn.addEventListener("click", () => {
-    count = 0
-    enableButtons();
-});
-
-// const winCheck = () => {
-//     for (let i of winningPattern) {
-//         let [element1, element2, element3] = [
-//             board[i[0]].innerText,
-//             board[i[1]].innerText,
-//             board[i[2]].innerText,
-//         ];
-//         if (element1 != "" && (element2 != "") & (element3 != "")) {
-//             if (element1 == element2 && element2 == element3) {
-//                 winFunction(element1)
-//             }
-//         }
-//     }
-// };
-
- board.forEach((element) => {
-    element.addEventListener("click", () => {
-        if (turnX) {
-            turnX = false;
-            element.innerText = "X";
-            element.disabled =  true;
-        } else {
-            turnX = true;
-            element.innerText = "O";
-            element.disabled = true;
-        }
-        count += 1;
-        if (count == 9) {
-            drawFunction();
-        }
-        winCheck();
-        });
-    });
- 
-    window.onload = enableButtons;
 
